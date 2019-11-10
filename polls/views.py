c3306c4ecde61bd0eedca6bae440f7a830ebf93b
Nagesh Podilapu <nageswara.podilapu@scout24.com>
2019-11-10 18:20:19 +0100

    Add question and choice to polls app

--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Question
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
-    name = request.GET.get('name')
-    return HttpResponse("Hello, %s. You're at the polls index." % name)
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
 
 
 def lists(request):
     return HttpResponse("Polls list is coming soon here...")
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
